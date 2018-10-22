98c0463184e49c1ca49c548b15f15cc0ef35cf61
Michelle M <mutanwho@gmail.com>
2018-10-22 16:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let nodes = document.querySelectorAll('.ranked')
+  let nodes = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < nodes.length; i += 1){
 }
 
 function deepestChild(){
