ec3f6387844c016cc5b2e48fff8a07b3052ae40a
Michelle M <mutanwho@gmail.com>
2018-10-22 16:52:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const nums = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < nums.length; i++) {
+    nums[i].innerHTML = parseInt(nums[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+ var deepest = document.querySelectorAll('#grand-node div');
+  return deepest[deepest.length - 1]
+}
 
