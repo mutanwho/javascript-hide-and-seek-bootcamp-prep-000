111711646e236e30e83519465394bef96c9eb25f
Michelle M <mutanwho@gmail.com>
2018-10-22 16:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-function getFirstSelector(selector){
-  return document
-    .getElementById('app')
-    .querySelector('#nested .target')
-}
 
-function nestedTarget(){
-  
-}
-
-function increaseRankBy(n){
-  
-}
-
-function deepestChild(){
-  
-}
 
