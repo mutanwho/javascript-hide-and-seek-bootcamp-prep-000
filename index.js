e49ad842b124415d44bc46c6991bc09e8cd7a53b
Michelle M <mutanwho@gmail.com>
2018-10-22 16:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var items =  document.getElementById('app')
-  document.querySelectorAll('.ranked-list li')
+  var items =  document.getElementById('app').querySelectorAll('.ranked-list li')
   for (let i = 0; i < items.length; i += 1){
     let num = parseInt(nodes[i].innerHTML);
     nodes[i].innerHTML = (num + n);
