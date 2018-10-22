dd137acdd8ab5ab7c59512ecc2924f93c95f82cb
Michelle M <mutanwho@gmail.com>
2018-10-22 16:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+function getFirstSelector(selector){
+  return document
+    .getElementById('app')
+    .querySelector('#nested .target')
+}
 
+function nestedTarget(){
+  
+}
+
+function increaseRankBy(n){
+  
+}
+
+function deepestChild(){
+  
+}
 
