948324d7e8bb8839a7fe61904dcf2b71ae13b0e7
Michelle M <mutanwho@gmail.com>
2018-10-22 16:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.querySelector('#nested').querySelector('.target')
+  return document.
 }
 
 function increaseRankBy(n){
